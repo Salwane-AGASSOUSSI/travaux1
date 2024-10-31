3d1b6830ce84408f1b6356e53142393de26746e2
Alexis Lessnick <alexis.lessnick@umontreal.ca>
2024-10-31 12:58:44 -0400

    ajout de partie B

--- soumission/MonAventure.java
+++ soumission/MonAventure.java
@@ -1,5 +1,63 @@
 package soumission;
 
-public class MonAventure { //TODO: extends ? implements? 
+import code_squelette.Aventure;
+import code_squelette.Labyrinthe;
+import code_squelette.Piece;
+import code_squelette.RencontreType;
 
+public class MonAventure extends Aventure {
+
+    /**
+     * Constructeur necessaire.
+     * @param c le labyrinthe de l'aventure.
+     */
+    public MonAventure(Labyrinthe c) {
+        super(c);
+    }
+
+    /**
+     * On va chercher le labyrinthe.
+     */
+    Labyrinthe labyrintheActif = this.getLabyrinthe();
+
+    @Override
+    public boolean estPacifique(){
+        for (Piece piece : labyrintheActif.getPieces()){
+            if (piece.getRencontreType() == RencontreType.MONSTRE || piece.getRencontreType() == RencontreType.BOSS){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean contientDuTresor() {
+        for (Piece piece : labyrintheActif.getPieces()){
+            if (piece.getRencontreType() == RencontreType.TRESOR){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int getTresorTotal(){
+        int compteurTresor = 0;
+        for (Piece piece : labyrintheActif.getPieces()){
+            if (piece.getRencontreType() == RencontreType.TRESOR){
+                compteurTresor++;
+            }
+        }
+        return compteurTresor;
+    }
+
+    @Override
+    public boolean contientBoss(){
+        for (Piece piece : labyrintheActif.getPieces()){
+            if (piece.getRencontreType() == RencontreType.BOSS){
+                return true;
+            }
+        }
+        return false;
+    }
 }
